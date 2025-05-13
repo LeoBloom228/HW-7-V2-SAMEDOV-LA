5ffe25b737e8a2311fee3640c1c6597a3525c19f
samedov.l <samedov.leon@outlook.com>
2025-05-13 16:36:50 +0300

    Задание 2 в рамках ДЗ 7

--- Homework-7/file1.cpp
+++ Homework-7/file1.cpp
@@ -1,29 +1,99 @@
+//#include <iostream>
+//#include <cstddef>  
+//#include <cstring>  
+
+//task 1
+//int main() 
+//{
+    
+    //char* str = new char[256];
+
+    //std::cout << "Enter a line (a line must not exeed 255 symbols): ";
+    //std::cin.getline(str, 256);
+
+    
+    ///char* p = str;
+
+    //while (*p != '\0') 
+    //{
+        //if (*p >= '0' && *p <= '9') 
+        //{
+            //*p = 'a' + (*p - '0'); 
+        //}
+        //++p;
+    //}
+
+    //std::cout << "Results is as follows: " << str << std::endl;
+
+    //delete[] str; // Releasing memory
+
+    //return 0;
+//}
+
+//task 2
+
 #include <iostream>
 #include <cstddef>  
-#include <cstring>  
+#include <cstring>
 
+// The function of removing all adjacent identical characters
+char* removeadjacentduplicates(char* str) 
+{
+    size_t len = strlen(str);
+    if (len < 2) return str;
+
+    // Allocating a buffer with a reserve (max length of the original string)
+    char* result = new char[len + 1];
+    char* write = result;
+    char* read = str;
+
+    while (*read != '\0') 
+    {
+        // If the current and next symbols are the same - skip both
+        if (*(read + 1) != '\0' && *read == *(read + 1)) 
+        {
+            // Skip the same pair
+            char curr = *read;
+            while (*read == curr && *(read + 1) == curr) 
+            {
+                ++read;
+            }
+            // Skip the last of the pair
+            ++read;
+        }
+        else 
+        {
+            *write = *read;
+            ++write;
+            ++read;
+        }
+    }
+
+    *write = '\0'; // completing the line
+
+    // reallocating memory to the new length
+    size_t newLen = strlen(result);
+    char* compressed = new char[newLen + 1];
+    strcpy_s(compressed, newLen + 1, result);
+
+    delete[] result;
+    delete[] str; // Deleteing the old line
+
+    return compressed;
+}
 
 int main() 
 {
-    
+    // Allocating memory for the original string
     char* str = new char[256];
 
-    std::cout << "Enter a line (a line must not exeed 255 symbols): ";
+    std::cout << "Please, enter a string: ";
     std::cin.getline(str, 256);
 
-    
-    char* p = str;
+    // Removing adjacent pairs
+    str = removeadjacentduplicates(str);
 
-    while (*p != '\0') 
-    {
-        if (*p >= '0' && *p <= '9') 
-        {
-            *p = 'a' + (*p - '0'); 
-        }
-        ++p;
-    }
-
-    std::cout << "Results is as follows: " << str << std::endl;
+    std::cout << "The result is: " << str << std::endl;
 
     delete[] str; // Releasing memory
 
